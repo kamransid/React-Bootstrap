230d274ef8e46496e238b9ce6467497934f0be68
md kamran siddique <ksid0001@ipv6-test.local>
2019-01-19 19:14:21 +0530

    Adding style

--- src/ButtonGroup.jsx
+++ src/ButtonGroup.jsx
@@ -9,11 +9,15 @@ class ButtonGroupExample extends React.Component{
         }
     }
     render(){
+        const style={
+            marginTop: '10px'
+        }
         return(
-           <ButtonGroup>
-               <Button bsStyle='primary'>Left</Button>
-               <Button bsStyle='warning'>Middle</Button>
-               <Button bsStyle='success'>Right</Button>
+
+           <ButtonGroup style={style}>
+               <Button  bsStyle='primary'>Left</Button>
+               <Button  bsStyle='warning'>Middle</Button>
+               <Button  bsStyle='success'>Right</Button>
            </ButtonGroup>
         )
     }
