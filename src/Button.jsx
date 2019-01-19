ce399a0fd5e3f1d493677fb0e0a473aedb583708
md kamran siddique <ksid0001@ipv6-test.local>
2019-01-19 15:13:02 +0530

    Adding style

--- src/Button.jsx
+++ src/Button.jsx
@@ -9,14 +9,41 @@ class ButtonExample extends React.Component{
         }
     }
     render(){
+        const block = {
+            display: 'block',
+            marginTop: '5px',
+            //fontFamily: 'source-code-pro, Menlo, Monaco, Consolas,'
+        }
         return (
-            
                 <div>
-               <Button>Default</Button>
+               <Button className='hola'>Default</Button>
                {/* <div style={{display: 'inline', background:'red'}}>Hi This is me</div> */}
-               <Button style={{display:'block'}} bsStyle='primary'>Primary</Button>
+               <Button style={block} bsStyle='primary'>Primary</Button>
+               <Button style={block} bsStyle='success'>Success</Button>
+               <Button style={block} bsStyle='warning'>Warning</Button>
+               <Button style={block} bsStyle='danger'>Danger</Button>
+               
+
+                <Button active bsStyle='primary' style={block}>Active</Button>
+
+
+
+               <Button bsSize='large' style={block} bsStyle='primary'>Large</Button>
+         
+               <Button style={block} bsStyle='primary' block>Block</Button>
+
+               <Button style={block} bsStyle='warning' bsSize='small' >Small</Button>
+
+               <Button bsStyle='info' bsSize='xsmall' style={block}>XSmall</Button>
+
+               
+               
+               
+               
+               
                </div>
-            
+
+
         )
     }
 
